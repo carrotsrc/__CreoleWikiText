fbf0a6c50bed4975bd3e8d954ad2ea86ae2a7bd7
Charlie FG <charlie@carrotsrc.org>
2014-06-23 23:09:01 +0100

    Updated readme

--- entry.c
+++ entry.c
@@ -16,7 +16,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "creole_parse.h"
-#define TEST_FILE "/home/charlie/development/c/creole/test2.cre"
+#define TEST_FILE "/home/charlie/development/c/creole/test.cre"
 int main(int argc, char *argv[])
 {
 	unsigned int sz = 0;
